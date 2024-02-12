2f59fcdbbe43eceb5ef257886c316bf6492c5fed
danielhuynh081 <159817045+danielhuynh081@users.noreply.github.com>
2024-02-12 10:16:34 -0800

    Example file upload

---
+++ main (1).cpp
@@ -0,0 +1,121 @@
+#include "class.h"
+
+//Daniel Huynh, CS302 Karla Fant, January 10th 2023,
+//This program is a racing program allowing the user to race against a bot in one of three snow sports.
+//using a LLL CLL and Vector in the application
+//
+
+int main()
+{
+	//Variable declaration
+	string name ="";
+	char start, select ='\0';
+	vector<Node> recentplayers;
+	int age =0;
+
+	//Races
+	SnowboardingRace SnowRace; //CLL
+	SnowmobileRace MobileRace; // LLL
+	SkiRace Skiracing; // LLL
+	playerlist recentlist; //vector
+
+	//Start menu1
+	do
+	{
+		cout <<"welcome to the snow racing program, press x to start";
+		cin >>start;
+		cin.ignore(100, '\n');
+		if(toupper(start) != 'X'){
+			cout <<"\nWrong character entered, try again\n";
+		}
+
+	}while(toupper(start) != 'X');
+	//Start menu2, this navigates our program
+	while(toupper(start) =='X')
+	{
+		menu();
+		cin >> select;
+		cin.ignore(100, '\n');
+		if(toupper(select) =='A') //Snowboarding
+		{
+			//register into recent players
+			cout <<"what is your name?" << endl;
+			cin>>name;
+			cin.ignore(100, '\n');
+			cout <<"what is your age?" << endl;
+			cin >>age;
+			cin.ignore(100, '\n');
+
+			cout <<"snowboarding selected";
+
+			snowboarding obj(name, age,1);
+			recentlist.addplayer(obj);
+			SnowRace.addplayer(obj);
+			//add NPC racers
+			SnowRace.addplayer(snowboarding("ryan", 12,2));
+			SnowRace.addplayer(snowboarding("emma", 12,3));
+			SnowRace.addplayer(snowboarding("cody", 12,4));
+			SnowRace.addplayer(snowboarding("jacob", 12,5));
+
+			SnowRace.racestart();
+
+		}
+		if(toupper(select) =='B') //Snowmobiling
+		{
+			cout <<"what is your name?" << endl;
+			cin>>name;
+			cin.ignore(100, '\n');
+			cout <<"what is your age?" << endl;
+			cin >>age;
+			cin.ignore(100, '\n');
+
+			cout <<"snowmobiling selected";
+			snowmobiling obj(name, age,1);
+			MobileRace.addplayer(obj);
+			MobileRace.setmobile();
+			//add NPC racers
+			MobileRace.addplayer(snowmobiling("ryan", 12,2));
+			MobileRace.addplayer(snowmobiling("emma", 12,3));
+			MobileRace.addplayer(snowmobiling("cody", 12,4));
+			MobileRace.addplayer(snowmobiling("jacob", 12,5));
+			MobileRace.racestart();
+
+
+		}
+		if(toupper(select) =='C') //Skiing
+		{
+			cout <<"what is your name?" << endl;
+			cin>>name;
+			cin.ignore(100, '\n');
+			cout <<"what is your age?" << endl;
+			cin >>age;
+			cin.ignore(100, '\n');
+
+			cout <<"skiing selected";
+			//add NPC racers
+			skiing obj(name, age,1);
+			Skiracing.addplayer(obj);
+			//add NPC racers
+			Skiracing.addplayer(skiing("ryan", 12,2));
+			Skiracing.addplayer(skiing("emma", 12,3));
+			Skiracing.addplayer(skiing("cody", 12,4));
+			Skiracing.addplayer(skiing("jacob", 12,5));
+			Skiracing.racestart();
+
+
+
+
+		}	
+		if(toupper(select) =='D'){ //vector displaying
+			recentlist.displaylist();
+
+
+		}
+		if(toupper(select) =='E'){
+			start = 'z';
+			cout <<"\nQuitting application\n";
+		}
+	}
+
+	return 0;
+}
